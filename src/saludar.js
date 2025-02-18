d939cd51b2c546623c19fef6de38e09569bcfcc4
GatoCoder282 <diegomvaldez19@gmail.com>
2025-02-18 00:53:38 -0400

    Saludar segun la hora

--- src/saludar.js
+++ src/saludar.js
@@ -1,3 +1,14 @@
 export function saludar(nombre) {
-    return `Hola ${nombre}`;
+    const hora = new Date().getHours();
+  let saludo;
+
+  if (hora < 12) {
+    saludo = "Buenos días";
+  } else if (hora < 18) {
+    saludo = "Buenas tardes";
+  } else {
+    saludo = "Buenas noches";
+  }
+
+  return `${saludo} ${nombre}`;
   }
--- src/saludar.test.js
+++ src/saludar.test.js
@@ -7,5 +7,14 @@ describe("Función saludar", () => {
   test("Debe saludar por nombre", () => {
     expect(saludar("Juan")).toBe("Hola Juan");
   });
+  test("Debe saludar según la hora del día", () => {
+    const horaMañana = new Date();
+    horaMañana.setHours(9); // Simula que es de mañana
+    jest.useFakeTimers().setSystemTime(horaMañana);
+
+    expect(saludar("Ana")).toContain("Buenos días Ana");
+    
+    jest.useRealTimers();
+  });
 
 });
